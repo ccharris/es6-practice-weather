79546bdca59d0c9795a8df53ffdc7c2f8f0041cd
Carolyn Harris <ccharris27@gmail.com>
2017-12-08 14:36:44 -0500

    weather react app changes

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ class App extends Component {
     constructor (props){
         super(props);
         this.state = {
-            city: 'Seattle',
+            city: 'Indianapolis',
             description: ''
         }
     }
